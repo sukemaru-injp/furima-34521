2b281694f5a79267b2795bd73cc55cc6a672a591
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-24 11:16:56 +0900

    モデルに正規表現を記述した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A[a-z]+[0-9]\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: 'カタカナを使用してください' } do
+    validates :family_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
   validates :birthday, presence: true
 end

7b5b3362eaa8fb7b87826593d220a6e4ca3d394e
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-24 20:00:32 +0900

    newからcreate データベースの保存まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    @item.save
   end
   
 
   private
   def item_params
-    params.require(:item).permit(:name, :content, :category_id, :condition_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,22 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 
 
   belongs_to :user
   has_one_attached :image
   
-  validates :name, :content, :price, presence:true
+  validates :name, :content, presence:true
+  validates :image, presence: true
+  # 価格の設定は後ほど
+  validates :price, presence:true
 
 # アクティブハッシュバリデーション
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
-
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
 end

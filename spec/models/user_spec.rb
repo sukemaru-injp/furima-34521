a8acaa2653910f87a2f73a9d6e4ce41e8006491e
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-24 15:20:15 +0900

    コメントへの修正を行った

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
+      it "passwordが5文字以下だと登録できない" do
+        @user.password = "aaaa1"
+        @user.password_confirmation = "aaaa1"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+
       it "passwordが数字だけだと登録できない" do
         @user.password = "000000"
         @user.password_confirmation = "000000"
@@ -79,6 +87,40 @@ RSpec.describe User, type: :model do
         @user.birthday = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        friend = FactoryBot.build(:user)
+        friend.email = @user.email
+        friend.valid?
+        expect(friend.errors.full_messages).to include("Email has already been taken")
+      end
+
+      # 正規表現test
+
+      it "family_nameが英字では登録できない" do
+        @user.family_name = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name 全角文字を使用してください")
+      end
+
+      it "first_nameが英字では登録できない" do
+        @user.first_name = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name 全角文字を使用してください")
+      end
+
+      it "family_name_kanaが英字では登録できない" do
+        @user.family_name_kana = " aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana カタカナを使用してください")
+      end
+
+      it "first_name_kanaが英字では登録できない" do
+        @user.first_name_kana = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana カタカナを使用してください")
       end
 
     end

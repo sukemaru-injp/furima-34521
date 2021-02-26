3b7e702e13d97facc4cd5a6482b40d92f404c783
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-26 12:06:51 +0900

    詳細ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.includes(:user).order(id: "DESC")
@@ -19,12 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
   end
   
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

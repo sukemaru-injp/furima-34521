74a3975fc6ca0be793fc614069c92c71341a4927
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-26 14:44:06 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
   before_action :set_item, only: [:show, :edit]
   before_action :move_to_index, only: [:edit]
 
@@ -27,6 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to "/items/#{@item.id}" 
+    else
+      render :edit
+    end
+
   end
   
 

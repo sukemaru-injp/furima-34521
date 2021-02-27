8de4764943d420595c6dce133e212d1f311dce3e
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-27 18:19:21 +0900

    カード購入実装

--- Gemfile
+++ Gemfile
@@ -65,6 +65,6 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'active_hash'
-
+gem 'payjp'
 
 
--- app/controllers/gets_controller.rb
+++ app/controllers/gets_controller.rb
@@ -9,6 +9,7 @@ class GetsController < ApplicationController
   def create
     @get_address = GetAddress.new(get_params)
     if @get_address.valid?
+      pay_item
       @get_address.save
       redirect_to root_path
     else
@@ -19,7 +20,17 @@ class GetsController < ApplicationController
 
   private
   def get_params
-    params.require(:get_address).permit(:postal_code, :prefecture_id, :city, :address_name, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:get_address).permit(:postal_code, :prefecture_id, :city, :address_name, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: get_params[:token],
+      currency: 'jpy'  
+    )
   end
 end
 
